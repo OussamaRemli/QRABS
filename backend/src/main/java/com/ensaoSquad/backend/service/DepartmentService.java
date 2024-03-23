7ae4cd179eb26145c29de866caef6ca8770ea578
OmariYachouMakkour <omari20012003@gmail.com>
2024-03-23 03:25:45 +0000

    authentification

--- backend/src/main/java/com/ensaoSquad/backend/service/DepartmentService.java
+++ backend/src/main/java/com/ensaoSquad/backend/service/DepartmentService.java
@@ -1,6 +1,11 @@
 package com.ensaoSquad.backend.service;
 
+import com.ensaoSquad.backend.Model.Professor;
 import com.ensaoSquad.backend.dto.DepartmentDTO;
+import com.ensaoSquad.backend.dto.LoginDTO;
+import com.ensaoSquad.backend.repository.AuthRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -9,4 +14,23 @@ public interface DepartmentService {
     List<DepartmentDTO> getAllDepartment();
 
     DepartmentDTO findDepartmentByName(String name);
+
+    @Service
+    class AuthService {
+
+        @Autowired
+        private AuthRepository authRepository;
+
+        public boolean authenticate(LoginDTO loginDTO) {
+            Professor professor = authRepository.findByEmail(loginDTO.getEmail());
+
+            // Vérifier si l'utilisateur avec l'email donné existe dans la base de données
+            if (professor == null) {
+                return false;
+            }
+
+            // Vérifier si le mot de passe correspond
+            return professor.getPassword().equals(loginDTO.getPassword());
+        }
+    }
 }

c7e6794faafe1dc039c91d92b7c7d16bf2998a31
MohamedOMARI <omari20012003@gmail.com>
2024-03-24 17:58:17 +0000

    fix error

--- backend/src/main/java/com/ensaoSquad/backend/repository/SessionRepository.java
+++ backend/src/main/java/com/ensaoSquad/backend/repository/SessionRepository.java
@@ -1,7 +1,3 @@
 package com.ensaoSquad.backend.repository;
-
-import com.ensaoSquad.backend.model.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session,Long> {
+public interface SessionRepository {
 }

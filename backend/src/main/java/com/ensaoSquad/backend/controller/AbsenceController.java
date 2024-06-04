be5f07cb16695bd666e5d7c203958335d0986f1f
MouadMakkour <mouadmakkour1900@gmail.com>
2024-06-04 22:40:31 +0200

    absence count total just by module and level

--- backend/src/main/java/com/ensaoSquad/backend/controller/AbsenceController.java
+++ backend/src/main/java/com/ensaoSquad/backend/controller/AbsenceController.java
@@ -137,6 +137,34 @@ public class AbsenceController {
 //
 //        return ResponseEntity.ok(totalCounts);
 //    }
+@GetMapping("/absence/count/total")
+public ResponseEntity<List<Long>> getAbsenceCountsTotal(
+        @RequestParam("moduleId") long moduleId,
+        @RequestParam("levelId") long levelId) {
+
+    Module module = moduleService.findById(moduleId);
+    Level level = levelService.findById(levelId);
+
+    Map<Student, Map<String, Long>> absenceCounts = absenceService.getAbsenceCountsByModuleAndLevel(module, level);
+
+    long totalCours = 0;
+    long totalTp = 0;
+    long totalTd = 0;
+
+    for (Map<String, Long> studentAbsence : absenceCounts.values()) {
+        // Accumulate counts for each session type
+        totalCours += studentAbsence.getOrDefault("cours", 0L);
+        totalTp += studentAbsence.getOrDefault("tp", 0L);
+        totalTd += studentAbsence.getOrDefault("td", 0L);
+    }
+    List<Long> totalCounts = new ArrayList<>();
+
+    totalCounts.add(totalCours);
+    totalCounts.add(totalTp);
+    totalCounts.add(totalTd);
+
+    return ResponseEntity.ok(totalCounts);
+}
 
     @GetMapping("/absence/details")
     public ResponseEntity<Map<Student, List<StudentAbsenceDTO>>> getAbsenceDetailsOfStudent(
